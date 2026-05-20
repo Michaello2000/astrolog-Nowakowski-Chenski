e7058e2946786a49cfc63850d2d8890cac5a7224
oskarnuch <heny79.ac@gmail.com>
2026-05-20 10:23:52 +0200

    Create ObjectDetails.tsx file with active objects props interface

---
+++ src/components/ObjectDetails.tsx
@@ -0,0 +1,8 @@
+﻿import type { CelestialObject } from '../types';
+
+interface ObjectDetailsProps {
+    activeObject: CelestialObject | null;
+}
+
+export default function ObjectDetails({ activeObject }: ObjectDetailsProps) {
+}
